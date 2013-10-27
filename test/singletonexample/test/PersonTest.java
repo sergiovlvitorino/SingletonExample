c6b922237eb475130644ae1b6007df3a811f928b
unknown <sergiovlvitorino@gmail.com>
2013-10-27 00:44:26 -0200

    modified:   .classpath 	new file:   test/singletonexample/test/PersonTest.java

---
+++ test/singletonexample/test/PersonTest.java
@@ -0,0 +1,14 @@
+package singletonexample.test;
+
+import singletonexample.Person;
+import junit.framework.TestCase;
+
+public class PersonTest extends TestCase{
+
+	public void testIfThereIsOnlyOneInstanceOfPerson(){
+		Person person1 = Person.getInstance();
+		Person person2 = Person.getInstance();
+		assertTrue( person1 == person2 );
+	}
+	
+}
