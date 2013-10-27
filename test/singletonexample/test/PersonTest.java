7decc68ef747fbeb67e08513afec0417eaa974ee
unknown <sergiovlvitorino@gmail.com>
2013-10-27 01:53:44 -0200

    modified:   test/singletonexample/test/PersonTest.java

--- test/singletonexample/test/PersonTest.java
+++ test/singletonexample/test/PersonTest.java
@@ -5,6 +5,10 @@ import junit.framework.TestCase;
 
 public class PersonTest extends TestCase{
 
+	public void testIfReturnIsNotNull(){
+		assertNotNull(Person.getInstance());
+	}
+	
 	public void testIfThereIsOnlyOneInstanceOfPerson(){
 		Person person1 = Person.getInstance();
 		Person person2 = Person.getInstance();
